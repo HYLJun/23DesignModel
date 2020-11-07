46e7c53928e17740c4dbd6da89cfd5980c5b795c
HYLJun <826184122@qq.com>
2020-11-07 17:41:50 +0800

    工厂方法模式 Factory Model

---
+++ 创建型模式/src/A1工厂方法模式FactoryModel.java
@@ -0,0 +1,48 @@
+public class A1工厂方法模式FactoryModel {
+    public interface Sender{
+        public void sender();
+    }
+    //两个实现类
+    public class MailSender implements Sender {
+
+        @Override
+        public void sender() {
+            System.out.println("this is mail sender");
+        }
+    }
+
+    public class SmsSender implements Sender {
+
+        @Override
+        public void sender() {
+            System.out.println("this is sms sender");
+        }
+    }
+
+    //工厂接口
+    public interface Provider {
+        public Sender produce();
+    }
+    //工厂类
+    public class SendMailFactory implements Provider {
+
+        @Override
+        public Sender produce() {
+            return new MailSender();
+        }
+    }
+    public class SendSmsFactory implements Provider {
+
+        @Override
+        public Sender produce() {
+            return new SmsSender();
+        }
+    }
+
+    //测试类
+    public static void main(String[] args) {
+       Provider provider = new A1工厂方法模式FactoryModel().new SendMailFactory();
+       Sender sender = provider.produce();
+       sender.sender();
+    }
+}
