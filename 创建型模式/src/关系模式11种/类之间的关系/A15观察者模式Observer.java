ad1f20088c9c59d0bb25a2f7420528d6d630469e
HYLJun <826184122@qq.com>
2020-11-08 22:46:04 +0800

    观察者模式Observer

--- 创建型模式/src/关系模式11种/类之间的关系/A15观察者模式Observer.java
+++ 创建型模式/src/关系模式11种/类之间的关系/A15观察者模式Observer.java
@@ -32,7 +32,6 @@ class Observer2 implements Observer {
 }
 
 /*Subject接口及实现类*/
-
 interface Subject {
     /*增加观察者*/
     void add(Observer observer);
@@ -53,15 +52,11 @@ abstract class AbstractSubject implements Subject {
         vector.add(observer);
     }
 
-    ;
-
     /*删除观察者*/
     @Override
     public void remove(Observer observer) {
         vector.remove(observer);
     }
-
-    ;
 
     /*通知所有的观察者*/
     @Override
